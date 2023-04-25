8e0800f1fb88ef88ae625c16e3f234683f831552
takamasa-like-curry <sugaharatakamasa@localhost>
2023-04-25 10:35:58 +0900

    ブランドのマスタ化に伴い、ブランドIDがnullの場合の代替えint値をenumに設定しました。

--- src/main/java/com/example/common/NullValue.java
+++ src/main/java/com/example/common/NullValue.java
@@ -2,14 +2,14 @@ package com.example.common;
 
 public enum NullValue {
 
-	CATEGORY_ID(-1),PAGE(1),SHIPPING(-1),;
-	
+	CATEGORY_ID(-1), PAGE(1), SHIPPING(-1), BRAND_ID(-1);
+
 	private final int value;
-	
+
 	private NullValue(int value) {
 		this.value = value;
 	}
-	
+
 	public int getValue() {
 		return value;
 	}
