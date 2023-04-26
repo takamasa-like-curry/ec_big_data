1a81faa8fff455fa8a068de5f460906fa6a6d630
takamasa-like-curry <sugaharatakamasa@localhost>
2023-04-26 10:44:23 +0900

    保守性を高めるため、1階層下のカテゴリリストを取得するAPIをこちらに定義しました。 また、1階層下のカテゴリを持たない場合に削除を行う機能を実装しました。

--- src/main/java/com/example/controller/RakusItemsApiController.java
+++ src/main/java/com/example/controller/RakusItemsApiController.java
@@ -1,11 +1,16 @@
 package com.example.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.domain.Category;
 import com.example.service.RakusItemsApiService;
 
 @RestController
@@ -15,12 +20,21 @@ public class RakusItemsApiController {
 	@Autowired
 	private RakusItemsApiService service;
 
-
-
 	@GetMapping("/check-category-name")
 	public ResponseEntity<Boolean> checkChildCategory(String categoryName, Integer categoryId) {
 		Boolean result = service.checkCategoryNameDuplication(categoryName, categoryId);
 		return ResponseEntity.ok(result);
+	}
+
+	@GetMapping("/pick-up-subordinate-category-list")
+	public Map<String, List<Category>> pickUpSubordinateCategoryList(Integer categoryId) {
+
+		Map<String, List<Category>> map = new HashMap<>();
+		if (categoryId != null) {
+			map.put("categoryList", service.pickUpSubordinateCategoryList(categoryId));
+		}
+		return map;
+
 	}
 
 }
