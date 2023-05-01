25629827450a7bdb1311fe41a62fc687645044d9
takamasa-like-curry <sugaharatakamasa@localhost>
2023-05-01 16:11:52 +0900

    developブランチにて作業をしてしまっていたので切り替えます。

---
+++ src/main/java/com/example/domain/FilterOfShowCategory.java
@@ -0,0 +1,18 @@
+package com.example.domain;
+
+/**
+ * カテゴリ検索フィルタ
+ * 
+ * @author sugaharatakamasa
+ *
+ */
+public class FilterOfShowCategory {
+
+	/** カテゴリ名 */
+	private String name;
+	/** カテゴリ名 */
+	private int level;
+	/** 先祖カテゴリID */
+	private int ancestorId;
+
+}
