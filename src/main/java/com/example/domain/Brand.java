486f2ce2a7e17bcd2026ae39731e9943a5c4ed3f
takamasa-like-curry <sugaharatakamasa@localhost>
2023-05-09 14:42:57 +0900

    nullを許容していた部分を改修しました。

--- src/main/java/com/example/domain/Brand.java
+++ src/main/java/com/example/domain/Brand.java
@@ -9,7 +9,7 @@ public class Brand {
 
 	private int id;
 	private String name;
-	
+
 	private Brand() {
 		this.id = NullValue.BRAND_ID.getValue();
 	}
@@ -18,32 +18,39 @@ public class Brand {
 		this.id = NullValue.BRAND_ID.getValue();
 		this.name = name;
 	}
-	private Brand(Integer id) {
+
+	private Brand(int id) {
 		setId(id);
 	}
-	
-	private Brand(Integer id,String name) {
+
+	private Brand(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
-	
-	
 
 	public static Brand createWithName(String name) {
 		return new Brand(name);
 	}
+
 	public static Brand createWithId(Integer id) {
+		if (id == null) {
+			id = NullValue.BRAND_ID.getValue();
+		}
 		return new Brand(id);
 	}
+
 	public static Brand create() {
 		return new Brand();
 	}
-	
-	public static Brand createWithIdAndName(Integer id,String name) {
+
+	public static Brand createWithIdAndName(Integer id, String name) {
+		if (id == null) {
+			id = NullValue.BRAND_ID.getValue();
+		}
 		return new Brand(id, name);
 	}
 //////////////////セッター・セッター///////////////////////////////////
-	
+
 	public void setId(Integer id) {
 		if (id == null) {
 			this.id = NullValue.BRAND_ID.getValue();
