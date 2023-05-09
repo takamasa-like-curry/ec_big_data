570eab65fbfdab1df2543fbf2bed0c9fcf079b27
takamasa-like-curry <sugaharatakamasa@localhost>
2023-05-09 11:00:32 +0900

    カテゴリ削除機能追加に伴い、カテゴリ閉包テーブルにメソッドを追加しました。

--- src/main/java/com/example/mapper/CategoryTreePathsMapper.java
+++ src/main/java/com/example/mapper/CategoryTreePathsMapper.java
@@ -1,11 +1,17 @@
 package com.example.mapper;
 
-
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface CategoryTreePathsMapper {
 
-	void insert(@Param("ancestorId") int ancestorId,@Param("descendantId") int descendantId);
+	void insert(@Param("ancestorId") int ancestorId, @Param("descendantId") int descendantId);
+
+	/**
+	 * カテゴリとその子孫カテゴリを削除(論理削除).
+	 * 
+	 * @param categoryId 削除するカテゴリID
+	 */
+	void logicalDeleteCategoryWithDescendants(int categoryId);
 }
